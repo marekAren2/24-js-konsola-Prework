a5449cee42bfcbc9158d6e5e280e6ce322a16120
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-12 20:24:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(
+  "Uwielbiam JavaScript!")
+/* Opis
+
+1. Stwórz plik main.js
+2. Napisz w nim kod, który wyświetla w konsoli napis "Uwielbiam JavaScript!".
+
+Dodaj plik do repozytorium zdalnego i podeślij link do sprawdzenia.
+ */
